3c248c69e278139b4364eb69e47aa0677d7797d6
Ging <YanM16M@gmail.com>
2023-09-30 15:09:06 +0200

    socket

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { format } from "date-fns";
+
 import { Member, Message, Profile } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+import { useChatSocket } from "@/hooks/use-chat-socket";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
     member: Member & {
@@ -37,6 +43,8 @@ export const ChatMessages = ({
     type
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`;
 
     const {
         data,
@@ -50,6 +58,7 @@ export const ChatMessages = ({
         paramKey,
         paramValue
     });
+    useChatSocket({ queryKey, addKey, updateKey });
 
     if (status === "loading") {
         return (
@@ -84,9 +93,19 @@ export const ChatMessages = ({
                 {data?.pages?.map((group, i) => (
                     <Fragment key={i}>
                         {group.items.map((message: MessageWithMemberWithProfile) => (
-                            <div key={message.id}>
-                                {message.content}
-                            </div>
+                            <ChatItem
+                                key={message.id} 
+                                id={message.id} 
+                                currentMember={member}
+                                member={message.member}
+                                content={message.content}
+                                fileUrl={message.fileUrl}
+                                deleted={message.deleted}
+                                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                                isUpdated={message.updatedAt !== message.createdAt}
+                                socketUrl={socketUrl}
+                                socketQuery={socketQuery}
+                            />  
                         ))}
                     </Fragment>
                 ))}
